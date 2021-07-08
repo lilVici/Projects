1e1776b36bb4528304fa58e74e2e8b146b1ee772
Ventsislav Monev <vencihm@abv.bg>
2021-07-08 10:44:05 +0300

    Second variant to command line project

--- ComandLineArgument.cpp
+++ ComandLineArgument.cpp
@@ -1,53 +1,38 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <sstream>
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-    vector<int> numbers;
-    std::cout << "Please enter the numbers : ";
-    std::string input = "";
-    std::getline(std::cin, input);
-    string word = "";
-    for (auto x : input)
-    {
-        int inumber = 0;
-        stringstream snumber(word);
-        snumber >> inumber;
-        if (x == ' ')
-        {
-            numbers.emplace_back(inumber);
-            word = "";
-        }
-        else
-        {
-            word = word + x;
-        }
-    }
-    std::cout << "In the numbers ";
-    for (auto x : numbers)
-    {
-        std::cout << x << ' ';
-    }
-    std::cout << "\n";
+	vector<int> numbers;
+	int number = 0;
 
-    int max = numbers[0];
-    int min = numbers[0];
-    for (auto num : numbers)
-    {
-        if (max < num)
-        {
-            max = num;
-        }
-        if (min > num)
-        {
-            min = num;
-        }
-    }
-    std::cout << "The maximum number is " << max << std::endl;
-    std::cout << "The minimum number is " << min << std::endl;
-    return 0;
+	std::cout << "The numbers are : ";
+
+	for (int i = 1; i < argc; i++)
+	{
+		std::cout << argv[i] << " ";
+		stringstream convertNumber(argv[i]);
+		convertNumber >> number;
+		numbers.emplace_back(number);
+	}
+	std::cout << endl;
+
+	int max = numbers[0];
+	int min = numbers[0];
+	for (auto num : numbers)
+	{
+		if (max < num)
+		{
+			max = num;
+		}
+		if (min > num)
+		{
+			min = num;
+		}
+	}
+	std::cout << "The maximum number is " << max << std::endl;
+	std::cout << "The minimum number is " << min << std::endl;
 }
